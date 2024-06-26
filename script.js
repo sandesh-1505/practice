4ec22925a1499353cdb89f589904574774fe7503
sandesh-1505 <sandesh.y.jogdande@gamil.com>
2024-06-26 19:17:09 +0530

    first.commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// console.log("Hello, myslef sandesh Jogdande");
+// a=2;
+// b=4;
+
+// console.log("ab =",a**b);
+
+
+/*
+ age=14;
+
+if(age>18){
+    console.log("you can vote!");
+}else{
+    console.log("you can not vote!");
+}
+
+console.log("\n")
+result = age>18 ? "can vote" : "cant vote" ;
+console.log(result);
+
+//alert("Hello");
+
+prompt("hello");
+*/
+
+let num = prompt("Enter a no.");
+
+if(num%5==0){
+    console.log("Number is multiple of 5");
+}else{
+    console.log("Number is not multiple of 5");
+}
